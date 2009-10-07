d589a0081bf618854fc7b12f7fd0e6924201c9e7
Nolan Wright <nwright@appcelerator.com>
2009-10-07 10:58:27 -0700

    merge

--- Resources/modules/dashboard/js/dashboard.js
+++ Resources/modules/dashboard/js/dashboard.js
@@ -9,12 +9,12 @@ Dashboard = {
 
 
 // register module
-TiDev.registerModule({
-	name:'dashboard',
-	displayName: 'Dashboard',
-	perspectives:['projects'],
-	html:'dashboard.html',
-	idx:0,
-	callback:Dashboard.eventHandler
-});
+// TiDev.registerModule({
+// 	name:'dashboard',
+// 	displayName: 'Dashboard',
+// 	perspectives:['projects'],
+// 	html:'dashboard.html',
+// 	idx:0,
+// 	callback:Dashboard.eventHandler
+// });
 
